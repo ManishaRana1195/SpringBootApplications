d9406c92dbdfce1f026001ffef80963f1d561662
ManishaRana1195 <manisha1195@gmail.com>
2019-05-31 12:03:16 -0500

    initialized dribble clone application

---
+++ Dribble-Clone/src/main/java/com/example/manisharana/DribbleClone/DribbleCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.manisharana.DribbleClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DribbleCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DribbleCloneApplication.class, args);
+	}
+
+}
---
+++ Dribble-Clone/src/test/java/com/example/manisharana/DribbleClone/DribbleCloneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.manisharana.DribbleClone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DribbleCloneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
