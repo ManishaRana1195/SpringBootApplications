52c38bcff49e4b1fe5d25a5ecf2547a2cd97167a
ManishaRana1195 <manisha1195@gmail.com>
2019-07-01 13:14:45 -0500

    mflix - initial commit

---
+++ mflix/src/test/java/mflix/api/manisha_rana_tests/ConnectionTest.java
@@ -0,0 +1,18 @@
+package mflix.api.manisha_rana_tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ConnectionTest {
+
+  @Test
+  public void createMongoClientAndGetMflixDatabase(){
+    
+  }
+}
